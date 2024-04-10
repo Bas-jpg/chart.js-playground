9e8594720a819a92288fc1a7d169cb4c4af44a33
bas-jpg <302806549@student.rocmondriaan.nl>
2024-04-10 10:26:40 +0200

    rjkergerek

---
+++ main.js
@@ -0,0 +1,20 @@
+const firstCv = document.querySelector(".first-cv");
+
+new Chart(firstCv, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'number of votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
